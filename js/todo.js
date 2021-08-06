c0239cc84a3baec95eac0e34fdb29a5d1334bf0e
jsky119 <87289235+jsky119@users.noreply.github.com>
2021-08-06 13:17:32 +0900

    Feat: Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -13,12 +13,15 @@ function saveToDos() {
 function removeToDo(event) {
   const li = event.target.parentElement;
   li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
 }
 
 function addToDo(newTodo) {
   const li = document.createElement("li");
+  li.id = newTodo.id;
   const span = document.createElement("span");
-  span.innerText = newTodo;
+  span.innerText = newTodo.text;
   const button = document.createElement("i");
   button.className += "fas fa-times fa-2x";
   button.addEventListener("click", removeToDo);
@@ -31,8 +34,12 @@ function handleToDoSubmit() {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
-  toDos.push(newTodo);
-  addToDo(newTodo);
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  addToDo(newTodoObj);
   saveToDos();
 }
 
